a768b577b72215579fb30da314f1e7c3ba920c86
pakdad-mousavi <pakdad.mousavi@gmail.com>
2026-05-11 22:47:07 +0800

    Add capacitor to the project and update gitignore file.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pakdad.splittr',
+  appName: 'Splittr',
+  webDir: 'dist'
+};
+
+export default config;
