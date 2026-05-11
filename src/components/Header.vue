006a6755a4561b50fe56472bf9eeea65d14f5e4c
pakdad-mousavi <pakdad.mousavi@gmail.com>
2026-05-11 17:42:48 +0800

    Add a basic version of the header component.

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="border-b p-4 border-gray-300/15">
+    <div class="flex items-center">
+      <div class="flex gap-x-4 items-center mr-auto">
+        <img src="/logo.svg" alt="Logo" class="w-7 h-7" />
+        <h1 class="font-playfair font-bold uppercase tracking-tight text-2xl">Splittr</h1>
+      </div>
+      <div
+        class="w-6 h-6 rounded-full border border-electric-green bg-electric-green/20 flex items-center justify-center text-sm"
+      >
+        P
+      </div>
+    </div>
+  </header>
+</template>
