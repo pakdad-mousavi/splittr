7d134e97a23a5b22693fc96d534b20540a6b3a56
pakdad-mousavi <pakdad.mousavi@gmail.com>
2026-05-12 23:17:51 +0800

    Update header text color.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,11 @@
 <template>
-  <header class="border-b p-4 border-gray-300/15">
+  <header class="p-4 border-gray-300/15 border-b">
     <div class="flex items-center">
       <div class="flex gap-x-4 items-center mr-auto">
         <img src="/logo.svg" alt="Logo" class="w-7 h-7" />
-        <h1 class="font-playfair font-bold uppercase tracking-tight text-2xl">Splittr</h1>
+        <h1 class="font-playfair font-bold uppercase tracking-tight text-2xl text-electric-green">
+          Splittr
+        </h1>
       </div>
       <div
         class="w-6 h-6 rounded-full border border-electric-green bg-electric-green/20 flex items-center justify-center text-sm"
