a13744129c3d48c2973ec0c12f4304c227d617bf
pakdad-mousavi <pakdad.mousavi@gmail.com>
2026-05-13 16:55:51 +0800

    Allow users to sign in with 3 predefined accounts as well.

--- src/views/login/Welcome.vue
+++ src/views/login/Welcome.vue
@@ -8,8 +8,32 @@ const email = ref('');
 const errorText = ref<string | null>(null);
 const router = useRouter();
 
+interface PREDEFINED_ACCOUNT {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const PREDEFINED_ACCOUNTS = [
+  {
+    name: 'James',
+    email: 'james@example.com',
+    password: '123456',
+  },
+  {
+    name: 'Emily',
+    email: 'emily@example.com',
+    password: '123456',
+  },
+  {
+    name: 'Gray',
+    email: 'gray@example.com',
+    password: '123456',
+  },
+] satisfies PREDEFINED_ACCOUNT[];
+
 // Handle email signups / logins
-const onContinueWithEmail = async () => {
+const continueWithEmail = async () => {
   if (email.value.length === 0) {
     errorText.value = 'Please enter your email.';
     return;
@@ -21,11 +45,24 @@ const onContinueWithEmail = async () => {
 
   if (error) {
     errorText.value = error.message;
-    console.log(error);
     return;
   }
 
   router.push(`/login/otp?email=${email.value}`);
+};
+
+const signInWithPredefinedAccount = async (acc: PREDEFINED_ACCOUNT) => {
+  const { error } = await supabase.auth.signInWithPassword({
+    email: acc.email,
+    password: acc.password,
+  });
+
+  if (error) {
+    errorText.value = error.message;
+    return;
+  }
+
+  router.push('/');
 };
 </script>
 
@@ -45,7 +82,7 @@ const onContinueWithEmail = async () => {
     />
     <button
       class="cursor-pointer text-xs bg-electric-green hover:bg-electric-green/80 rounded-md py-1.5 text-cursed-black flex gap-x-1 justify-center active:translate-y-px duration-200"
-      @click="onContinueWithEmail"
+      @click="continueWithEmail"
     >
       <Email class="stroke-cursed-black w-4 h-4 stroke-1!"></Email>
       <span class="">Continue with Email</span>
@@ -60,19 +97,12 @@ const onContinueWithEmail = async () => {
 
     <div class="flex flex-col gap-y-2">
       <button
+        v-for="(acc, index) in PREDEFINED_ACCOUNTS"
+        :key="index"
+        @click="signInWithPredefinedAccount(acc)"
         class="cursor-pointer text-xs bg-neutral-800 hover:bg-neutral-800/80 rounded-md py-3 flex gap-x-1 justify-center active:translate-y-px duration-200 border border-electric-green/30"
       >
-        <span class="">Sign in with James</span>
-      </button>
-      <button
-        class="cursor-pointer text-xs bg-neutral-800 hover:bg-neutral-800/80 rounded-md py-3 flex gap-x-1 justify-center active:translate-y-px duration-200 border border-electric-green/30"
-      >
-        <span class="">Sign in with Emily</span>
-      </button>
-      <button
-        class="cursor-pointer text-xs bg-neutral-800 hover:bg-neutral-800/80 rounded-md py-3 flex gap-x-1 justify-center active:translate-y-px duration-200 border border-electric-green/30"
-      >
-        <span class="">Sign in with Gray</span>
+        <span class="">Sign in with {{ acc.name }}</span>
       </button>
     </div>
   </div>
