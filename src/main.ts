b544bdaa03d40466b328455ff59638525496e3d7
pakdad-mousavi <pakdad.mousavi@gmail.com>
2026-05-16 13:53:48 +0800

    Add pinia to the project.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createPinia } from 'pinia';
+import router from './router';
 
-const app = createApp(App)
+const pinia = createPinia();
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.use(pinia);
+app.mount('#app');
